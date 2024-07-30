c2c0d615ddaedd592a85bf8a8a04e7638780cd95
Michiel <michiel_m@live.nl>
2024-07-30 09:37:03 +0200

    "Fish cleanup"

--- dot_config/fish/functions/cm-all.fish
+++ dot_config/fish/functions/cm-all.fish
@@ -1,10 +1,40 @@
-function cm-all --wraps='chezmoi add'
-    chezmoi add ~/.config/alacritty/ $argv
-    chezmoi add ~/.config/hypr/ $argv
-    chezmoi add ~/.config/oh-my-posh/ $argv
-    chezmoi add ~/.config/user-scripts/ $argv
-    chezmoi add ~/.config/rofi/ $argv
-    chezmoi add ~/.config/waybar/ $argv
-    chezmoi add ~/.config/swaylock $argv
+function test-cm
+    argparse h/help c/cd p/push -- $argv
+    or return
+ 
+    set present $PWD
+
+    if set -ql _flag_push
+        if not test -n "$argv[1]"
+            echo "Missing parameter: Message. Please use the following format: cm-all [-h|--help] [-s|--cd] [-p|--push %message%]"
+            return -1
+        end
+    end
+
+    chezmoi add ~/.config/alacritty/
+    chezmoi add ~/.config/hypr/
+    chezmoi add ~/.config/oh-my-posh/
+    chezmoi add ~/.config/user-scripts/
+    chezmoi add ~/.config/rofi/
+    chezmoi add ~/.config/waybar/
+    chezmoi add ~/.config/swaylock
+    chezmoi add ~/.config/fish
+
+    # If -h or --help is given, we print a little help text and return
+    if set -ql _flag_help
+        echo "cm-all [-h|--help] [-s|--cd] [-p|--push %message%]"
+        return 0
+    end
+
+    if set -ql _flag_cd
+        chezmoi cd
+    end
+
+    if set -ql _flag_push
+        gitid
+        chezmoi git add .
+        chezmoi git -- commit -m \"$argv[1]\"
+        chezmoi git push
+    end
 end
 
